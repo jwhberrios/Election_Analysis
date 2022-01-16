ced3a5bd4d937f8a1d503d933bed5264facb4152
jwhberrios <94324519+jwhberrios@users.noreply.github.com>
2022-01-16 00:50:19 -0500

    Update PyPoll_Challenge.py

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -2,8 +2,6 @@
 import csv
 import os
 
-from nbformat import write
-
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
